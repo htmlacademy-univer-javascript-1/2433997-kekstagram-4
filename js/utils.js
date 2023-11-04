a88c6afb965bab81ae7f7c6f7c826f2a04155f1c
Abulk79 <129064915+Abulk79@users.noreply.github.com>
2023-11-04 12:42:11 +0500

    Задание 5.14

---
+++ js/utils.js
@@ -0,0 +1,25 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomId = (a, b) => {
+  const arrayId = [];
+  return function () {
+    let id = getRandomInteger(a, b);
+
+    while (arrayId.includes(id)) {
+      id = getRandomInteger(a, b);
+    }
+
+    arrayId.push(id);
+    return id;
+  };
+};
+
+const getRandomArrayElement = (elements) =>
+  elements[getRandomInteger(0, elements.length - 1)];
+
+export { getRandomInteger, getRandomId, getRandomArrayElement };
