07404637788dc18baac1ac6005e882086f7c50da
Abulk79 <129064915+Abulk79@users.noreply.github.com>
2023-11-23 12:39:22 +0500

    Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-import {
-  createPhotoDescription,
-  SIMILAR_PHOTO_DESCRIPTION_COUNT,
-} from './data.js';
-
-const similarPhotoDescription = Array.from(
-  { length: SIMILAR_PHOTO_DESCRIPTION_COUNT },
-  createPhotoDescription
-);
+import './thumbnails.js';
